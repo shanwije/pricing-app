8b86b680ddd14719db811eaa537c2cacb3318eb8
Shan Wijenayaka <shanwijenayaka@outlook.com>
2021-06-30 07:32:21 +0530

    axios setup completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import {baseUrl, urls} from "./constants";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // eslint-disable-next-line no-unused-vars
+    const [productsAndPrices, setProductsAndPrices] = useState(null);
+
+    useEffect(() => {
+        fetchProductsAndPrices();
+    }, []);
+
+    const fetchProductsAndPrices = () => {
+        axios.get(`${baseUrl}${urls.GET_PRODUCTS_AND_PRICES}`)
+            .then(res => {
+                setProductsAndPrices(res.data);
+            })
+            .catch(err => console.error(err));
+    }
+
+    return (
+        <div className="App">
+            {/*<header className="App-header">*/}
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    {JSON.stringify(productsAndPrices)}
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            {/*</header>*/}
+            <body>
+
+            </body>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const baseUrl = "http://localhost:8080"
+
+export const urls = {
+    GET_PRODUCTS_AND_PRICES: "/products-and-prices",
+    GET_PRICE: "/price",
+}
