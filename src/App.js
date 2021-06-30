78064a8a021bb2b8e36bafabb39000e49b3114c5
Shan Wijenayaka <shanwijenayaka@outlook.com>
2021-06-30 14:03:39 +0530

    Calculator completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {baseUrl, urls} from "./constants";
-import {Col, Container, Navbar, Row, Table} from 'react-bootstrap';
+import {Button, Card, Col, Container, Form, Navbar, Row, Table} from 'react-bootstrap';
 
 function App() {
 
-    // eslint-disable-next-line no-unused-vars
     const [productsAndPrices, setProductsAndPrices] = useState([]);
+    const [quantity, setQuantity] = useState(0);
+    const [product, setProduct] = useState('');
+    const [isCartons, setCartons] = useState(false);
+    const [calculatedPrice, setCalculatedPrice] = useState(0.00);
+    const [isPriceFactorsChanged, setPriceFactorsChanged] = useState(true);
 
     useEffect(() => {
         fetchProductsAndPrices();
@@ -17,84 +21,173 @@ function App() {
         axios.get(`${baseUrl}${urls.GET_PRODUCTS_AND_PRICES}`)
             .then(res => {
                 setProductsAndPrices(res.data);
+                setProduct(productsAndPrices[0].product);
+            })
+            .catch(err => console.error(err));
+    }
+
+    const handleProductSelect = (e) => {
+        setPriceFactorsChanged(true);
+        const selectedProduct = productsAndPrices
+            .map(({product}) => product)
+            .find(product => product.id == e.target.value);
+        setProduct(selectedProduct);
+    }
+    const handleSetQuantity = (e) => {
+        setPriceFactorsChanged(true);
+        setQuantity(e.target.value);
+    }
+
+    const handleSetPackageType = (isCartons) => {
+        setPriceFactorsChanged(true);
+        setCartons(isCartons);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(quantity, product, isCartons);
+
+        const {cartonPrice, unitsPerCarton} = product;
+
+        const params = {
+            cartonPrice,
+            unitsPerCarton,
+            quantity,
+            isCartons
+        }
+
+        axios.get(`${baseUrl}${urls.GET_PRICE}`, {params})
+            .then(res => {
+                console.log(res);
+                setPriceFactorsChanged(false);
+                setCalculatedPrice(res.data.price);
             })
             .catch(err => console.error(err));
     }
 
     return (
-        <>
-            <Navbar bg="light">
-                <Navbar.Brand>My Assignment Store</Navbar.Brand>
+        <Container fluid={true}>
+            <Navbar sticky={true}>
+                <Navbar.Brand>My Store</Navbar.Brand>
             </Navbar>
-            <Container>
-                <Row md={1}>
-                    <title>Store</title>
-                    <Col md='1'>
-                        <div className='table-wrapper'>
-                            <Table striped bordered hover variant="dark">
-                                <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Product name</th>
-                                    <th>
-                                        <tr>Prices</tr>
-                                        <tr>
-
-                                        </tr>
-                                    </th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {
-                                    productsAndPrices.length && productsAndPrices.map(({product, pricesList}, i) => {
-                                        console.log(pricesList)
+            <Row sm={12} className='mt-3'>
+                <title>Store</title>
+                <Col sm={7}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Products</Card.Title>
+                            <div className='table-wrapper'>
+                                <Table striped bordered hover variant="light" responsive>
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Product name</th>
+                                        <th>Price Info</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {productsAndPrices.length
+                                    && productsAndPrices.map(({product, pricesList}, i) => {
                                         return (
-                                            <tr>
+                                            <tr key={i}>
                                                 <td>{i}</td>
                                                 <td>{product.name}</td>
                                                 <td>
-
-                                                    {/*<th>*/}
-                                                    {/*    <td>index</td>*/}
-                                                    {/*    <td>price</td>*/}
-                                                    {/*</th>*/}
-                                                    <div className='price-col'>
-                                                        <Table scrollable={true}>
+                                                    <Card className='price-col'>
+                                                        <Table size="sm" responsive>
                                                             <thead>
                                                             <tr>
                                                                 <th>Item Count</th>
                                                                 <th>Calculated Price</th>
                                                             </tr>
-                                                            {pricesList.map((price, i) => {
-                                                                return (<tr>
-                                                                    <td>{i}</td>
-                                                                    <td>{price}</td>
-                                                                </tr>);
-                                                            })}
                                                             </thead>
+                                                            <tbody>
+                                                            {pricesList
+                                                                .filter((price, i) => i !== 0)
+                                                                .map((price, i) => (<tr key={i + 1}>
+                                                                        <td>{i + 1}</td>
+                                                                        <td>{price.toFixed(2)}</td>
+                                                                    </tr>)
+                                                                )}
+                                                            </tbody>
                                                         </Table>
-                                                    </div>
-
-
-                                                    {/*</section>*/}
+                                                    </Card>
                                                 </td>
                                             </tr>
                                         );
                                     })
-                                }
+                                    }
+                                    </tbody>
+                                </Table>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col sm={5}>
+                    <Card className='d-grid gap-2'>
+                        <Card.Body>
+                            <Card.Title> Calculate Price </Card.Title>
+                            {productsAndPrices.length && (
+                                <>
+                                    <Form onSubmit={(e) => handleSubmit(e)}>
+                                        <Form.Group controlId="form.product">
+                                            <Form.Label>Product</Form.Label>
+                                            <Form.Control as="select"
+                                                          value={product.id}
+                                                          onChange={e => handleProductSelect(e)} custom>
+                                                {productsAndPrices.map(
+                                                    ({product}) => (
+                                                        <option key={product.id} id={product.id}
+                                                                value={product.id}>{product.name}</option>))}
+                                            </Form.Control>
+                                        </Form.Group>
 
-                                </tbody>
-                            </Table>
-                        </div>
-
-                    </Col>
-                    <Col md='1'>
-
-                    </Col>
-                </Row>
-            </Container>
-        </>
-
+                                        <Form.Group controlId="form.quantity">
+                                            <Form.Label>Quantity</Form.Label>
+                                            <Form.Control type="number" placeholder="quantity" min="0" step="1"
+                                                          value={quantity}
+                                                          onChange={e => handleSetQuantity(e)}/>
+                                        </Form.Group>
+                                        <Form.Group as={Row}>
+                                            <Form.Label as="legend" column sm={5}>
+                                                Package
+                                            </Form.Label>
+                                            <Col sm={10}>
+                                                <Form.Check
+                                                    type="radio"
+                                                    label="Cartons"
+                                                    name="package"
+                                                    id="carton"
+                                                    checked={isCartons}
+                                                    onChange={e => handleSetPackageType(true)}
+                                                />
+                                                <Form.Check
+                                                    type="radio"
+                                                    label="Units"
+                                                    name="package"
+                                                    id="unit"
+                                                    checked={!isCartons}
+                                                    onChange={e => handleSetPackageType(false)}
+                                                />
+                                            </Col>
+                                        </Form.Group>
+                                        <Button variant="primary" type="submit" className='custom-full-width-button'>
+                                            Calculate
+                                        </Button>
+                                        {!isPriceFactorsChanged && (<Form.Group className='mt-3'>
+                                            <Form.Label>Price</Form.Label>
+                                            <Form.Control value={calculatedPrice.toFixed(2)}
+                                                          placeholder="Price"
+                                                          disabled/>
+                                        </Form.Group>)}
+                                    </Form>
+                                </>
+                            )}
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
